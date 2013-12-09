1b0a0b0496a247715a7e7e71576e735a31a5bdab
Luca Guidi <me@lucaguidi.com>
2013-12-09 11:41:04 +0100

    Bump version to v3.2.5

--- lib/redis/activesupport/version.rb
+++ lib/redis/activesupport/version.rb
@@ -1,5 +1,5 @@
 class Redis
   module ActiveSupport
-    VERSION = '3.2.4'
+    VERSION = '3.2.5'
   end
 end
--- test/redis/activesupport/version_test.rb
+++ test/redis/activesupport/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Redis::ActiveSupport::VERSION do
   it 'returns current version' do
-    Redis::ActiveSupport::VERSION.must_equal '3.2.4'
+    Redis::ActiveSupport::VERSION.must_equal '3.2.5'
   end
 end
