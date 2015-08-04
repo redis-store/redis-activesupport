25ef4731140301c082e1e7d4f675b5671cb165dd
Ryan Bigg <git@ryanbigg.com>
2015-08-05 07:24:04 +1000

    Bump to 4.1.1

--- test/redis/activesupport/version_test.rb
+++ test/redis/activesupport/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Redis::ActiveSupport::VERSION do
   it 'returns current version' do
-    Redis::ActiveSupport::VERSION.must_equal '4.1.0'
+    Redis::ActiveSupport::VERSION.must_equal '4.1.1'
   end
 end
